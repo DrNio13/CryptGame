c98f8248b9b22fd85e1838b44f0f5fc3706faf28
DrNio13 <vandergeorge@hotmail.com>
2015-09-05 22:32:50 +0300

    multiple solutions in array

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
 
     /**
      *
+     * @type {number}
+     * how many words the user has found - initial value is 0
+     */
+    CryptoGame.counter = 0;
+
+
+
+    /**
+     *
      * @param sumUp is a function that adds the selection of the characters of the user
      * @param subtractChar is a function that removes the latest char from the current selection of the user
      */
@@ -15,6 +24,7 @@ $(document).ready(function(){
 
             $(this).toggleClass('selected');
 
+            // get the value of the current selection
             var thisValue = $(this).text();
 
             if ( $(this).hasClass('selected') ) {
@@ -26,7 +36,11 @@ $(document).ready(function(){
 
         });
 
+
+
     };
+
+
 
     /**
      *
@@ -35,9 +49,12 @@ $(document).ready(function(){
     CryptoGame.sumUp = function(self){
 
         CryptoGame.sumOfLetters += self;
-        //console.log(CryptoGame.sumOfLetters);
+
+        // evaluate the current word
         CryptoGame.evaluate(CryptoGame.sumOfLetters);
     };
+
+
 
     /**
      * 
@@ -45,22 +62,54 @@ $(document).ready(function(){
     CryptoGame.subtractChar = function(){
 
         CryptoGame.sumOfLetters = CryptoGame.sumOfLetters.slice(0,-1);
-        //console.log(CryptoGame.sumOfLetters);
+
+        // evaluate current word
         CryptoGame.evaluate(CryptoGame.sumOfLetters);
     };
 
-    CryptoGame.evaluate = function(currentWord){
-        //console.log(currentWord);
-        var solution = 'oxi';
 
-        if (currentWord == solution) {
-            console.log('vrikes pithani lysi');
+
+    /**
+     *
+     * @param currentWord is the sum of chars that the user clicked
+     * evaluate() works fine if you click in the right or opposite order NOT random order.
+     **/
+    CryptoGame.evaluate = function(currentWord){
+
+        // multiple solutions
+        var solutions = ['AGAPI', 'BABE', 'PISI'];
+
+        var reversedWord = currentWord.split('').reverse().join();
+
+
+
+        function searchInSolutions(currentSolution, index, solution){
+
+            var splitSolution = currentSolution.split('');
+
+            if ( currentWord == currentSolution ||  reversedWord == splitSolution  ) {
+
+                console.log('vrikes pithani lysi');
+                CryptoGame.success();
+            }
+            else {
+
+                console.log('sunexise na psaxneis');
+            }
+
         }
-        else {
-            console.log('sunexise na psaxneis');
-        }
+
+        solutions.forEach(searchInSolutions);
+
+
     };
 
+
+    CryptoGame.success = function(){
+
+
+
+    };
 
     CryptoGame.selectLetters(CryptoGame.sumUp, CryptoGame.subtractChar);
 
